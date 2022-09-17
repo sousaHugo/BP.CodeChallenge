04bdaa19f6458d7af0c1f6f267219ee3c601ffe8
hugosousacplt <hs@complyteq.com>
2022-09-17 14:27:21 +0100

    Commit Tests

--- BluePrism.TechnicalTest.Tests/Application/ApplicationTests.cs
+++ BluePrism.TechnicalTest.Tests/Application/ApplicationTests.cs
@@ -6,21 +6,102 @@ namespace BluePrism.TechnicalTest.Tests.Application
 {
     public class ApplicationTests
     {
-
-      
-
+        private readonly string _urlBluePrismFilesTestsPath = @"C:\BluePrismFiles";
+        private readonly string _fileName = @"TesteMain.txt";
+        private readonly List<string> _linesSuccess = new List<string>() { "Spin", "Spit", "Spat", "Spot", "Span" };
+        private IFileOperation _fileOperation;
+        private string _fullFileName = string.Empty;
+        private string _outputName = "OutputFile.txt";
+        private string _outputFullName = string.Empty;
         [SetUp]
         public void Setup()
         {
-            
+            if (!Directory.Exists(_urlBluePrismFilesTestsPath))
+                Directory.CreateDirectory(_urlBluePrismFilesTestsPath);
+
+            _fileOperation = new FileOperation();
+            _fullFileName = $"{_urlBluePrismFilesTestsPath}\\{_fileName}";
+            _outputFullName = $"{_urlBluePrismFilesTestsPath}\\{_outputName}";
+            _fileOperation.Create(_fullFileName, _linesSuccess);
         }
 
         [Test]
-        public void ReadAllLinesNoProvidedFilePath()
+        public void ApplicationSuccess()
         {
-            //Program.Main(new string[4] { "Element 1", "Element 2", "Element 3", "Element 4"});
+            var expectedResult = new List<string>() { "Spin", "Spit", "Spot" }.AsEnumerable();
 
-            //Assert.That(1, Is.EqualTo(1));
+            Program.Main(new string[4] { _fullFileName, "Spin", "Spot", _outputName });
+
+            Assert.That(File.Exists(_outputFullName), Is.True);
+
+            var resultData = _fileOperation.Get(_outputFullName);
+
+            CollectionAssert.AreEqual(expectedResult, resultData);
+
+            File.Delete(_outputFullName);
+            File.Delete(_fullFileName);
+        }
+        [Test]
+        public void ApplicationSuccessWithResultFileNameExtension()
+        {
+            var expectedResult = new List<string>() { "Spin", "Spit", "Spot" }.AsEnumerable();
+            var outputFileName = $"{_urlBluePrismFilesTestsPath}\\OutPutFileWithExtention.txt";
+
+            Program.Main(new string[4] { _fullFileName, "Spin", "Spot", "OutPutFileWithExtention.txt" });
+
+            Assert.That(File.Exists(outputFileName), Is.True);
+
+            var resultData = _fileOperation.Get(outputFileName);
+
+            CollectionAssert.AreEqual(expectedResult, resultData);
+
+            File.Delete(outputFileName);
+            File.Delete(_fullFileName);
+        }
+        [Test]
+        public void ApplicationSuccessWithoutResultFileNameExtension()
+        {
+            var expectedResult = new List<string>() { "Spin", "Spit", "Spot" }.AsEnumerable();
+            var outputFileName = $"{_urlBluePrismFilesTestsPath}\\OutPutFileWithExtention.txt";
+
+            Program.Main(new string[4] { _fullFileName, "Spin", "Spot", "OutPutFileWithExtention" });
+
+            Assert.That(File.Exists(outputFileName), Is.True);
+
+            var resultData = _fileOperation.Get(outputFileName);
+
+            CollectionAssert.AreEqual(expectedResult, resultData);
+
+            File.Delete($"{outputFileName}.txt");
+            File.Delete(_fullFileName);
+        }
+        [Test]
+        public void ApplicationEmptyDictionaryUrl()
+        {
+            Program.Main(new string[4] { String.Empty, "Spin", "Spot", _outputName });
+
+            Assert.That(!File.Exists(_outputFullName), Is.True);
+        }
+        [Test]
+        public void ApplicationEmptyStartWord()
+        {
+            Program.Main(new string[4] { _fullFileName, string.Empty, "Spot", _outputName });
+
+            Assert.That(!File.Exists(_outputFullName), Is.True);
+        }
+        [Test]
+        public void ApplicationEmptyEndWord()
+        {
+            Program.Main(new string[4] { _fullFileName, "Spin", string.Empty, _outputName });
+
+            Assert.That(!File.Exists(_outputFullName), Is.True);
+        }
+        [Test]
+        public void ApplicationEmptyResultFileName()
+        {
+            Program.Main(new string[4] { _fullFileName, "Spin", "Spot", string.Empty });
+
+            Assert.That(!File.Exists(_outputFullName), Is.True);
         }
     }
 }
